0b5fb39b35738773da957a2437198d3672377189
zahra-suhail240 <zahrasuhail15@gmail.com>
2024-12-31 17:03:18 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Backend Developer", "Fullstack Developer", "Mobile and Web Developer"],
+    typeSpeed: 50, // Typing speed in milliseconds
+    backSpeed: 50, // Backspacing speed in milliseconds
+    backDelay: 1000, // Delay before backspacing in milliseconds
+    loop: true // Enable infinite loop
+});
